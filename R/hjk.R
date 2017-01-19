494f7b4a732742e987423ab6477ddb04235c207f
theGreatWhiteShark <thetruephil@googlemail.com>
2017-01-19 18:08:15 +0100

    reviewing the hjk procedure for GEV fitting but it makes some very bold steps in the beginning considering the shape parameter. If not tuned accordingly it is not usable in the GEV fitting. And I do not seethe point right now in tweaking yet another fitting procedure.

--- R/hjk.R
+++ R/hjk.R
@@ -2,8 +2,11 @@
 ##  h o o k e j e e v e s . R  Hooke-Jeeves Minimization Algorithm
 ##
 
-
-hjk <- function(par, fn, control = list(), ...) {
+## I will not use this one because it makes some very bold steps in the beginning considering the
+## shape parameter. If not tuned accordingly it is not usable in the GEV fitting. And I do not see
+## the point right now in tweaking yet another fitting procedure.
+hjk <- function( par = climex::likelihood.initials( x ), fn = climex::likelihood, x = x,
+                control = list(), ... ) {
     if (!is.numeric(par))
         stop("Argument 'par' must be a numeric vector.", call. = FALSE)
     n <- length(par)
@@ -27,15 +30,17 @@ hjk <- function(par, fn, control = list(), ...) {
 
 	scale <- if (maximize) -1 else 1
     fun <- match.fun(fn)
-    f <- function(x) scale * fun(x, ...)
-
+    ## MOD
+    f <- function(x) scale * fun( par, x )
+    ## /MOD
+    
     #-- Setting steps and stepsize -----
     nsteps <- floor(log2(1/tol))        # number of steps
     steps  <- 2^c(-(0:(nsteps-1)))      # decreasing step size
     dir <- diag(1, n, n)                # orthogonal directions
 
-    x <- par                            # start point
-    fx <- fbest <- f(x)                 # smallest value so far
+    x.up <- par                            # start point
+    fx <- fbest <- f(x.up)                 # smallest value so far
     fcount <- 1                         # counts number of function calls
 
     if (info) cat("step\tnofc\tfmin\txpar\n")   # info header
@@ -44,14 +49,15 @@ hjk <- function(par, fn, control = list(), ...) {
     ns <- 0
     while (ns < nsteps && fcount < maxfeval && abs(fx) < target) {
         ns <- ns + 1
-        hjs    <- .hjsearch(x, f, steps[ns], dir, fcount, maxfeval, target)
-        x      <- hjs$x
+        hjs    <- .hjsearch(x.up, f, steps[ns], dir, fcount, maxfeval, target)
+        browser()
+        x.up      <- hjs$x
         fx     <- hjs$fx
         sf     <- hjs$sf
         fcount <- fcount + hjs$finc
 
         if (info)
-            cat(ns, "\t",  fcount, "\t", fx/scale, "\t", x[1], "...\n")
+            cat(ns, "\t",  fcount, "\t", fx/scale, "\t", x.up[1], "...\n")
     }
 
     if (fcount > maxfeval) {
@@ -65,7 +71,7 @@ hjk <- function(par, fn, control = list(), ...) {
     }
 
     fx <- fx / scale                    # undo scaling
-    return(list(par = x, value = fx,
+    return(list(par = x.up, value = fx,
                 convergence = conv, feval = fcount, niter = ns))
 }
 
@@ -118,7 +124,12 @@ hjk <- function(par, fn, control = list(), ...) {
         fb <- fbold
         numf <- 0
     }
-
+    
+    ## MOD
+    ## this is a quite dirty solution but lets keep it for a moment
+    if ( is.nan( fb ) )
+        fb <- Inf
+    ## /MOD
     fx <- fb
     xt <- xc
     sf <- 0                             # do we find a better point ?
@@ -126,12 +137,22 @@ hjk <- function(par, fn, control = list(), ...) {
     fbold <- fx
     for (k in sample.int(n, n)) {       # resample orthogonal directions
         p <- xt + dirh[, k]
+        ## MOD
+        ## this is a quite dirty solution but lets keep it for a moment
         ft <- f(p)
+        if ( is.nan( ft ) )
+            ft <- Inf
+        ## /MOD
         numf <- numf + 1
-
+        
         if (ft >= fb) {
             p <- xt - dirh[, k]
             ft <- f(p)
+            ## MOD
+            ## this is a quite dirty solution but lets keep it for a moment
+            if ( is.nan( ft ) )
+                ft <- Inf
+            ## /MOD
             numf <- numf + 1
         }
         if (ft < fb) {
